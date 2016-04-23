6b837f373fbb7e7dd894050ac95bdab69fd3812c
hanhailong <hanhailong@58ganji.com>
2016-04-23 18:14:06 +0800

    初始化ReactNative

---
+++ app/src/main/java/com/hhl/reactnativeproject/MainActivity.java
@@ -0,0 +1,21 @@
+package com.hhl.reactnativeproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickMe(View v) {
+        Intent intent = new Intent(MainActivity.this, MyReactActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
